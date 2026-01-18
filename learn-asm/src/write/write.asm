7ac52f9f3134c693c0eb9ae9e3df1fce24fefc6f
Sckab <giulianodeamicis2009@gmail.com>
2026-01-18 18:12:45 +0100

    updated write.asm

--- learn-asm/src/write/write.asm
+++ learn-asm/src/write/write.asm
@@ -2,6 +2,9 @@ section .data
     string db "Hollow Knight", 0x0A
     len_string equ $ - string
 
+    peak db "è peak", 0x0A
+    len_peak equ $ - peak
+
 section .text
     global _start
 
@@ -11,6 +14,14 @@ _start:
 
     mov rsi, string
     mov rdx, len_string
+    syscall
+
+    ; reset the syscall parameters cuz it resets it when it's called
+    mov rdi, 1
+    mov rax, 1
+
+    mov rsi, peak
+    mov rdx, len_peak
     syscall
 
     mov rdi, 0
